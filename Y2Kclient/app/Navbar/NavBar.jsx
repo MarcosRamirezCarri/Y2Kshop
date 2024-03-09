aca544fc70ae73b65ddcbe17edf05c23dfb6a0fd
Marcos Ramirez <gellollo@outlook.com>
2024-03-09 14:10:12 -0300

    Aprendizaje

--- Y2Kclient/app/Navbar/NavBar.jsx
+++ Y2Kclient/app/Navbar/NavBar.jsx
@@ -1,19 +1,40 @@
-import { Text, StyleSheet, View } from "react-native"
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native"
 
-export default function NavBar () {
-    return(
-        <View style={styles.container}>
-            <Text>Navbar</Text>
-        </View>
-    )
-}
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       flexDirection: 'row',
       backgroundColor: '#333',
       alignItems: 'center',
       justifyContent: 'space-around',
-      height: 60
+      placeSelf:'start',
+      height: 70,
+      width: '100vw'
     },
+    navItem: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      navText: {
+        color: '#fff',
+        fontSize: 18,
+      },
   });
+
+export default function NavBar () {
+
+    
+    return(
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Inicio</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Perfil</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navItem}>
+        <Text style={styles.navText}>Configuración</Text>
+      </TouchableOpacity>
+        </View>
+    )
+}
