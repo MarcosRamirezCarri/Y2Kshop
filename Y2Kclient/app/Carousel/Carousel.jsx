300c6fad622985c4c302639efa6dbf620b41cf0e
Marcos Ramirez <gellollo@outlook.com>
2024-03-19 18:51:47 -0300

    x

--- Y2Kclient/app/Carousel/Carousel.jsx
+++ Y2Kclient/app/Carousel/Carousel.jsx
@@ -1,61 +1,84 @@
-"use client"
-import { StyleSheet, View, Image } from "react-native";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  EffectCoverflow,
-  A11y,
-} from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-cube";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import mockCarousel from "../mocks/mock";
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  SafeAreaView,
+  FlatList,
+  Animated,
+} from "react-native";
+import mockCarousel from '../mocks/mock'
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+const ANCHO_CONTENEDOR = width * 0.7;
+const ESPACIO_CONTENEDOR = (width - ANCHO_CONTENEDOR) / 2;
+const ESPACIO = 10;
+
 
 export default function Carousels() {
+
+  
   return (
-    <Swiper
-      style={styles.wrapper}
-      effect={"coverflow"}
-      centeredSlides={false}
-      pagination={true}
-      navigation={true}
-      spaceBetween={10}
-      coverflowEffect={{
-        rotate: 90,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
-    >
-      {mockCarousel.map((image) => (
-        <SwiperSlide>
-          <View key={image.id} style={styles.slide}>
-            <Image
-              source={{ uri: image.img }}
-              style={styles.image}
-              resizeMode="cover"
-            />
-          </View>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+ <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        snapToAlignment="start"
+        contentContainerStyle={{
+          paddingTop: 10,
+          paddingHorizontal: ESPACIO_CONTENEDOR,
+        }}
+        snapToInterval={ANCHO_CONTENEDOR}
+        decelerationRate={0}
+        scrollEventThrottle={16}
+        data={mockCarousel}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => {
+
+          return (
+            <View key={item.id} style={{ width: ANCHO_CONTENEDOR }}>
+              <View
+                style={{
+                  marginHorizontal: ESPACIO,
+                  padding: ESPACIO,
+                  borderRadius: 34,
+                  backgroundColor: "#fff",
+                  alignItems: "center",
+                  transform: [{ translateY: scrollY }],
+                }}
+              >
+                <Image source={{ uri: item.img }} style={styles.posterImage} />
+                <Text style={{ fontWeight: "bold", fontSize: 26 }}>
+                  {" "}
+                  Título
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: {},
-  slide: {
+  container: {
     flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
   },
-  image: {
+  posterImage: {
     width: "100%",
-    height: "100%",
+    height: ANCHO_CONTENEDOR * 1.2,
+    resizeMode: "cover",
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
   },
 });
